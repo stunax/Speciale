d49886081d9706f1a37f485552a6d735e38354d6
Christian Edsberg Moellgaard <cem@zangenberg.biz>
2018-02-26 15:23:45 +0100

    derp

---
+++ verify/sum_plot.py
@@ -0,0 +1,25 @@
+import h5py
+import pickle
+import numpy as np
+import config
+from tqdm import tqdm
+
+threshold = 10000
+keep_anno = True
+
+
+images = []
+for h5f in config.h5s:
+    print(h5f)
+    out = "%s has %%i that is only 0" % h5f
+    h5f = h5py.File(h5f, 'r+')
+    images.append([(h5f, key) for key in h5f.keys() if keep_anno or (key[
+        :3] == "sec" or key[:4] == "anno")])
+
+images = sum(images, [])
+
+sums = []
+for h5f, gname in tqdm(images):
+    sums.append(np.sum(h5f, gname))
+with open('verify/sum_plot.pkl', 'wb') as f:
+    pickle.dump(sums, f)
