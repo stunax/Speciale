c9eb9fb25afcfb6d85db0a07f874677c918f9539
Christian Edsberg Moellgaard <cem@zangenberg.biz>
2018-02-27 12:11:34 +0100

    derp

--- verify/make_hist.py
+++ verify/make_hist.py
@@ -18,12 +18,13 @@ df['z'] = [int(x.split('_')[0]) for x in df.fname]
 print(df.shape)
 print(df.columns.values)
 path = "verify/"
-p = ggplot(aes(x='z', y='sums', colour='dataset'), data=df)
 
 fn = 'sum_density.png'
+p = ggplot(aes(x='z', y='sums', colour='dataset'), data=df)
 p_dens = p + geom_point()
 p_dens.save(path + fn)
 
 fn = 'sum_hist.png'
+p = ggplot(aes(x='sums', colour='dataset'), data=df)
 p_hist = p + geom_histogram()
 p_hist.save(path + fn)
