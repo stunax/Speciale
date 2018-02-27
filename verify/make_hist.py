f2aa09f08d474223f5197e3e2c6a7248f308db59
Christian Edsberg Moellgaard <cem@zangenberg.biz>
2018-02-27 11:13:34 +0100

    derp

--- verify/make_hist.py
+++ verify/make_hist.py
@@ -6,9 +6,13 @@ with open('verify/sum_plot.pkl', 'rb') as f:
     data = pickle.load(f)
 data = zip(*data)
 df = pd.DataFrame(data={'sums': data[0], 'fname': data[1]})
+df['z'] = [int(x.split('_')[0]) for x in data[1]]
 
 path = "verify/"
-fn = 'sum_density.png'
-p = ggplot(aes(x='sums'), data=df)
+p = ggplot(aes(x='z', y='sums'), data=df)
 
+fn = 'sum_density.png'
+ggsave(p + geom_point(), filename=fn, path=path)
+
+fn = 'sum_density.png'
 ggsave(p, filename=fn, path=path)
--- verify/sum_plot.py
+++ verify/sum_plot.py
@@ -9,11 +9,11 @@ keep_anno = False
 
 
 images = []
-for h5f in config.h5s:
-    print(h5f)
-    out = "%s has %%i that is only 0" % h5f
-    h5f = h5py.File(h5f, 'r+')
-    images.append([(h5f, key) for key in h5f.keys() if keep_anno or (key[
+for h5fn in config.h5s:
+    print(h5fn)
+    out = "%s has %%i that is only 0" % h5fn
+    h5f = h5py.File(h5fn, 'r+')
+    images.append([(h5f, key, h5fn) for key in h5f.keys() if keep_anno or (key[
         :3] == "sec" or key[:4] == "anno")])
 
 images = sum(images, [])
