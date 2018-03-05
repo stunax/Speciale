78dfc2f3ccc7ec257b792ba97ff39579cfec07c4
Christian Edsberg Moellgaard <cem@zangenberg.biz>
2018-03-05 10:29:02 +0100

    tet

--- verify/make_hist_front.py
+++ verify/make_hist_front.py
@@ -27,15 +27,15 @@ df['t'] = [int(x.split('_')[0]) for x in df.fname]
 
 fn = out_extra + 'sum_density_z.png'
 p = ggplot(aes(x='z', y='sums', colour='dataset'), data=df)
-p_dens = p + geom_point() + xlab("z") + stat_smooth(colour='blue', span=0.2)
+p_dens = p + geom_point() + facet_wrap('dataset', ncol=3) + xlab("z")
 p_dens.save(path + fn)
 
 fn = out_extra + 'sum_density_t.png'
 p = ggplot(aes(x='t', y='sums', colour='dataset'), data=df)
-p_dens2 = p + geom_point() + xlab("time")
+p_dens2 = p + geom_point() + facet_wrap('dataset', ncol=3) + xlab("time")
 p_dens2.save(path + fn)
 
 fn = out_extra + 'sum_hist.png'
-p = ggplot(aes(x='sums', colour='dataset'), data=df)
-p_hist = p + geom_histogram() #+ facet_wrap('dataset', ncol=3)
+p = ggplot(aes(x='sums'), data=df)
+p_hist = p + geom_histogram() + facet_wrap('dataset', ncol=3)
 p_hist.save(path + fn)
