26039d95cf90fcf45636643f11911a2adaeb4812
Christian Edsberg Moellgaard <cem@zangenberg.biz>
2018-03-12 14:00:40 +0100

    derp

--- labels/kmeans.py
+++ labels/kmeans.py
@@ -23,7 +23,7 @@ Xtrain, ytrain = datam.handle_images(images)
 print(Xtrain.shape)
 print(n)
 
-model = KMeans(n_clusters=16)
+model = KMeans(n_clusters=16, n_jobs=10)
 pred = model.fit_transform(Xtrain).reshape((n, 1024**2))
 for i in range(pred.shape[0]):
     fname = "%s_kmeans.png" % (im_paths[i])
