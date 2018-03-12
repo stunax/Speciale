bf8f47926243eaa8da9974193fea4c4772723cfc
Christian Edsberg Moellgaard <cem@zangenberg.biz>
2018-03-12 13:07:11 +0100

    derp

--- labels/kmeans.py
+++ labels/kmeans.py
@@ -1,4 +1,4 @@
-from imageio import imread
+from imageio import imread, imwrite
 from sklearn.cluster import KMeans
 from ..src.Model_data import Model_data
 import glob
@@ -7,10 +7,12 @@ path = 'labels/'
 im_reg = '*.png'
 
 images = []
+im_paths = []
 
 for im in glob.glob(path + im_reg):
     if "anno" not in im:
         images.append(imread(im))
+        im_paths.append(im)
 
 
 n = len(images)
@@ -22,3 +24,6 @@ Xtrain, ytrain = datam.handle_images(images)
 
 model = KMeans(n_clusters=16)
 pred = model.fit_transform(Xtrain).reshape((n, 1024**2))
+for i in range(pred.shape[0]):
+    fname = "%s.anno_%i.png" % (im_paths[i], i)
+    imwrite(pred[i])
