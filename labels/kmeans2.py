94ec0735da427685e725ab960276fedd762b94d4
Christian Edsberg Moellgaard <cem@zangenberg.biz>
2018-03-13 14:43:25 +0100

    derp

--- labels/kmeans2.py
+++ labels/kmeans2.py
@@ -7,8 +7,8 @@ import config
 
 h5s = config.get_h5(ignore_1_2=True)
 
-datam = Model_data(kernel_size=(11, 11, 1),
-                   flat_features=True, from_h5=True, bag_size=9)
+datam = Model_data(kernel_size=(9, 9, 1), remove_unlabeled=False,
+                   flat_features=True, from_h5=True, bag_size=11)
 
 Xtrain, ytrain = datam.handle_images(h5s)
 sub_Xtrain = Xtrain[np.random.choice(Xtrain.shape[0], 3000000, False)]
@@ -25,6 +25,6 @@ for h5fn in config.h5s[2:]:
             continue
         image = datam.handle_images([(h5f, df)])
         pred = model.predict(image)
-        fname = "%s/kmeans_labels/%s_%s_kmeans_anno.png" % (
+        fname = "%skmeans_labels/%s_%s_kmeans_anno.png" % (
             config.data_path, h5fn[:1], df)
         imwrite(fname, pred)
