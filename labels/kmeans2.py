0f9c84865a0d91d9727becaa062f1065df3bdc58
Christian Edsberg Moellgaard <cem@zangenberg.biz>
2018-03-14 11:27:01 +0100

    derp

--- labels/kmeans2.py
+++ labels/kmeans2.py
@@ -4,6 +4,7 @@ from imageio import imwrite
 from sklearn.cluster import KMeans
 from model_data import Model_data
 import config
+import random
 
 h5s = config.get_h5(ignore_1_2=True)
 
@@ -13,7 +14,6 @@ datam = Model_data(
 
 Xtrain, ytrain = datam.handle_images(h5s)
 datam.annotation_groupname = ""
-# datam.from_h5 = False
 sub_Xtrain = Xtrain[np.random.choice(Xtrain.shape[0], 3000000, False)]
 model = KMeans(n_clusters=16, n_jobs=6)
 model.fit(sub_Xtrain)
@@ -24,10 +24,10 @@ X = []
 for h5fn in config.h5s[2:]:
     h5f = h5py.File(h5fn, 'r+')
     for df in h5f.keys():
-        if len(df) > 6:
+        if len(df) > 6 or random.uniform(0, 1) < 0.9:
             continue
         image, _ = datam.handle_images([(h5f, df)])
         pred = model.predict(image).astype(np.uint8).reshape(1024, 1024)
-        fname = "%s/kmeans_labels/%s_%s_kmeans_anno.png" % (
+        fname = "%s/kmeans_labels/%s_%s.png" % (
             config.data_path, h5fn[:1], df)
         imwrite(fname, pred)
