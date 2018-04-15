0f9a3e9879ebc997cb19997d33d8e5fec9e7eb91
Christian <cmollgaard@gmail.com>
2018-04-15 11:00:33 +0200

    to_h5.py working

--- labels/to_h5.py
+++ labels/to_h5.py
@@ -8,17 +8,16 @@ h5s = {str(fn): h5py.File(config.data_path + str(fn) + ".h5", "a")
        for fn in range(1, 6)}
 
 
-for full_fname in glob.glob(config.annotations_path_c):
+for full_fname in glob.glob(config.annotations_path_c + "*.png"):
     # fname 4.h5023_19*.png
-    print(fname)
     fname = full_fname.rsplit("/", 1)[1]
     # h5, fname = fname[:-4].split(".", 1)
     h5 = h5s[fname[0]]
     fname = fname[4:10]
     fname = config.c_anno_groupname + fname
+    print (fname, h5)
     if fname in h5:
         continue
-    print (fname, h5)
     image = imread(full_fname)
     annotation = read_tiff.get_mask(image)
-    h5.create_dataset(fname, data=annotation)
+    #h5.create_dataset(fname, data=annotation)
