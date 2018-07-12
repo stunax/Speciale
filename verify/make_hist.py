5b25d0fc65108bf3db449c7c76cb9c8137a6b0d9
Christian <cmollgaard@gmail.com>
2018-07-12 14:23:27 +0200

    derp

--- verify/make_hist.py
+++ verify/make_hist.py
@@ -29,15 +29,15 @@ fn = out_extra + 'sum_density_z.png'
 p = ggplot(aes(x='z', y='sums', colour='dataset'), data=df)
 p_dens = p + geom_point() + facet_wrap('dataset', ncol=3) + xlab("z axis") + \
     ylab("Sum of slice intensity")
-p_dens.save(path + fn, width=3, height=3)
+p_dens.save(path + fn, width=4, height=4)
 
 fn = out_extra + 'sum_density_t.png'
 p = ggplot(aes(x='t', y='sums', colour='dataset'), data=df)
 p_dens2 = p + geom_point() + facet_wrap('dataset', ncol=3) + xlab("Frames") + \
     ylab("Sum of slice intensity")
-p_dens2.save(path + fn, width=3, height=3)
+p_dens2.save(path + fn, width=4, height=4)
 
 fn = out_extra + 'sum_hist.png'
 p = ggplot(aes(x='sums'), data=df)
 p_hist = p + geom_histogram() + facet_wrap('dataset', ncol=3)
-p_hist.save(path + fn, width=3, height=3)
+p_hist.save(path + fn, width=4, height=4)
