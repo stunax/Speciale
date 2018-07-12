4164084432e95894771b4e5f5d249fbea191e2be
Christian <cmollgaard@gmail.com>
2018-07-12 14:28:04 +0200

    derp

--- verify/make_hist.py
+++ verify/make_hist.py
@@ -25,21 +25,21 @@ df['dataset'] = [x.split('/')[-1] for x in df['dataset']]
 df['z'] = [int(x.split('_')[1]) for x in df.fname]
 df['t'] = [int(x.split('_')[0]) for x in df.fname]
 
-ylabel = ylab("s            Sum of slice intensity          s")
+ylabel = ylab("Sum of slice intensity")
 
 fn = out_extra + 'sum_density_z.png'
 p = ggplot(aes(x='z', y='sums', colour='dataset'), data=df)
 p_dens = p + geom_point() + facet_wrap('dataset', ncol=3) + xlab("z axis") + \
     ylabel
-p_dens.save(path + fn, width=5, height=4)
+p_dens.save(path + fn, width=5.5, height=4)
 
 fn = out_extra + 'sum_density_t.png'
 p = ggplot(aes(x='t', y='sums', colour='dataset'), data=df)
 p_dens2 = p + geom_point() + facet_wrap('dataset', ncol=3) + xlab("Frames") + \
     ylabel
-p_dens2.save(path + fn, width=5, height=4)
+p_dens2.save(path + fn, width=5.5, height=4)
 
 fn = out_extra + 'sum_hist.png'
 p = ggplot(aes(x='sums'), data=df)
 p_hist = p + geom_histogram() + facet_wrap('dataset', ncol=3)
-p_hist.save(path + fn, width=5, height=4)
+p_hist.save(path + fn, width=5.5, height=4)
