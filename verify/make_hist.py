0f56a73e0a36e437e9fed840fd8a8fcb64474ad8
Christian <cmollgaard@gmail.com>
2018-07-12 14:28:48 +0200

    derp

--- verify/make_hist.py
+++ verify/make_hist.py
@@ -31,15 +31,15 @@ fn = out_extra + 'sum_density_z.png'
 p = ggplot(aes(x='z', y='sums', colour='dataset'), data=df)
 p_dens = p + geom_point() + facet_wrap('dataset', ncol=3) + xlab("z axis") + \
     ylabel
-p_dens.save(path + fn, width=5.5, height=4)
+p_dens.save(path + fn, width=6, height=5)
 
 fn = out_extra + 'sum_density_t.png'
 p = ggplot(aes(x='t', y='sums', colour='dataset'), data=df)
 p_dens2 = p + geom_point() + facet_wrap('dataset', ncol=3) + xlab("Frames") + \
     ylabel
-p_dens2.save(path + fn, width=5.5, height=4)
+p_dens2.save(path + fn, width=6, height=5)
 
 fn = out_extra + 'sum_hist.png'
 p = ggplot(aes(x='sums'), data=df)
 p_hist = p + geom_histogram() + facet_wrap('dataset', ncol=3)
-p_hist.save(path + fn, width=5.5, height=4)
+p_hist.save(path + fn, width=6, height=5)
