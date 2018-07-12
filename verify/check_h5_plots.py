c6b748f0e4744f5bde5a7483c5bd300879178172
Christian <cmollgaard@gmail.com>
2018-07-12 14:06:31 +0200

    derp

--- verify/check_h5_plots.py
+++ verify/check_h5_plots.py
@@ -17,13 +17,13 @@ def geom_plots(y, ylabel, labs, df):
 
     p_z_intensity = p + geom_point(aes(x='z')) + facet_wrap('fname', ncol=3)
     # p_z_intensity += geom_smooth()
-    p_z_intensity.save(path + "z_%s.png" % labs, width=1.8)
+    p_z_intensity.save(path + "z_%s.png" % labs, width=3, height=3)
 
     p = ggplot(aes(y=y), df) + ylab(ylabel)
 
     p_t_intensity = p + geom_point(aes(x='t')) + \
         facet_wrap('fname', ncol=3)
-    p_t_intensity.save(path + "t_%s.png" % labs, width=1.8)
+    p_t_intensity.save(path + "t_%s.png" % labs, width=3, height=3)
 
 
 geom_plots('intensity_sum', 'Image intensity sum', 'intensity', df)
